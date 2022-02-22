5cffd91a11a3ccd6569f0eaebf4e3b81a1f5cd9b
LINETH-03 <lperezh9@miumg.edu.gt>
2022-02-22 11:05:41 -0600

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] numbers={15,30,50,80,79,88};
+        System.out.println(Arrays.toString(numbers)+"\n");
+        int numSearch = 30;
+        Arrays.sort(numbers);
+
+
+    }
+    public static void BinarySearch(int[] arraynumbers, int numSearch){
+
+
+    }
+
+}
